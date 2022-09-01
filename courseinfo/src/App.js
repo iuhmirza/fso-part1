a5c83a243378b6f096396b5550481bdf74d9f2b7
ibtisam <iuhmirza@gmail.com>
2022-09-01 12:33:42 +0100

    Refactor Content into Part

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -3,8 +3,16 @@ const Header = (props) => (
 )
 
 const Content = (props) => (
+  <div>
+    <Part part ={props[0].part1} exercises={props[0].exercises1}/>
+    <Part part ={props[1].part2} exercises={props[1].exercises2}/>
+    <Part part ={props[2].part3} exercises={props[2].exercises3}/>
+  </div>
+)
+
+const Part = (props) => (
   <p>
-    {props.part} {props.exercises}
+  {props.part} {props.exercises}
   </p>
 )
 
@@ -23,9 +31,7 @@ const App = () => {
   return (
     <div>
       <Header name ={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content {...[{part1, exercises1}, {part2, exercises2}, {part3, exercises3}]}/>
       <Total total = {exercises1+exercises2+exercises3}/>
     </div>
   )
