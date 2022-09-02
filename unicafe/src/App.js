74a17cd2941a44a2eba89368161c09aa4f292b67
Ibtisam <81363048+iuhmirza@users.noreply.github.com>
2022-09-02 17:54:55 +0100

    Show results in html table

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -4,24 +4,29 @@ const Header = ({text}) => <h1>{text}</h1>
 
 const Button = ({text, handleVote}) => <button onClick={handleVote}>{text}</button>
 
-const Results = ({text, value}) => <p>{text} {value}</p>
+const Results = ({text, value}) => (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+)
 
 const Statistics = ({good, neutral, bad}) => {
   if(good+bad+neutral === 0) {
     return <p>No feedback given</p>
   } else {
     return (
-      <div>
-        <Header text={'statistics'}/>
+      <table>
+        <tbody>
+          <Results text={'good'} value={good}  />
+          <Results text={'neutral'} value={neutral}/>
+          <Results text={'bad'} value={bad} />
   
-        <Results text={'good'} value={good}  />
-        <Results text={'neutral'} value={neutral}/>
-        <Results text={'bad'} value={bad} />
-  
-        <Results text={'all'} value={good+neutral+bad}/>
-        <Results text={'average'} value={(good - bad)/(good+neutral+bad)}/>
-        <Results text={'positive'} value={good/(good+bad+neutral)}/>
-      </div>
+          <Results text={'all'} value={good+neutral+bad}/>
+          <Results text={'average'} value={(good - bad)/(good+neutral+bad)}/>
+          <Results text={'positive'} value={good/(good+bad+neutral)}/>
+        </tbody>
+      </table>
     )
   }
 }
@@ -40,7 +45,9 @@ const App = () => {
       <Button text={'neutral'} handleVote={() => setNeutral(neutral + 1)}/>
       <Button text={'bad'} handleVote={() => setBad(bad + 1)}/>
 
+      <Header text={'statistics'}/>
       <Statistics good = {good} neutral = {neutral} bad = {bad}/>
+      
     </div>
 
   )
