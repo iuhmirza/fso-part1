b80d351e8a51d2c3efd0e034d00aab68b229b01b
Ibtisam <81363048+iuhmirza@users.noreply.github.com>
2022-09-02 17:17:45 +0100

    Refactor statistics into its own component

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -4,8 +4,28 @@ const Header = ({text}) => <h1>{text}</h1>
 
 const Button = ({text, handleVote}) => <button onClick={handleVote}>{text}</button>
 
-
 const Results = ({text, value}) => <p>{text} {value}</p>
+
+const Statistics = ({good, neutral, bad}) => {
+  if(good+bad+neutral === 0) {
+    return <p>No feedback given</p>
+  } else {
+    return (
+      <div>
+        <Header text={'statistics'}/>
+  
+        <Results text={'good'} value={good}  />
+        <Results text={'neutral'} value={neutral}/>
+        <Results text={'bad'} value={bad} />
+  
+        <Results text={'all'} value={good+neutral+bad}/>
+        <Results text={'average'} value={(good - bad)/(good+neutral+bad)}/>
+        <Results text={'positive'} value={good/(good+bad+neutral)}/>
+      </div>
+    )
+  }
+}
+
 
 const App = () => {
   const [good, setGood] = useState(0)
@@ -20,15 +40,7 @@ const App = () => {
       <Button text={'neutral'} handleVote={() => setNeutral(neutral + 1)}/>
       <Button text={'bad'} handleVote={() => setBad(bad + 1)}/>
 
-      <Header text={'statistics'}/>
-
-      <Results text={'good'} value={good}  />
-      <Results text={'neutral'} value={neutral}/>
-      <Results text={'bad'} value={bad} />
-
-      <Results text={'all'} value={good+neutral+bad}/>
-      <Results text={'average'} value={(good - bad)/(good+neutral+bad)}/>
-      <Results text={'positive'} value={good/(good+bad+neutral)}/>
+      <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     </div>
 
   )
